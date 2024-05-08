ddd36c12cf2b5e14de2b3e253f9a0d0c00cb12bf
IT22099518 <nuwanisithara.com@gmail.com>
2024-05-08 11:56:36 +0530

    test3 added

--- test1.py
+++ test1.py
@@ -7,6 +7,8 @@ print(x)
 x = x + 6
 print(x)
 
+# -------------------------------------------------------
+
 # Strings
 a = 'pasindu'
 abc = 60
@@ -16,7 +18,32 @@ abc_number = 40
 t = abc + abc_number
 print(t)
 
+# ------------------------------------------------------------
+
 # get inputs
-name = input('What is your name : ')
-print(name)
-print('Hellooo ' + name)
+# name = input('What is your name : ')
+# print(name)
+# print('Hellooo ' + name)
+
+# -----------------------------------------------------------------
+
+# Strings
+s = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+print(s[0])
+print(s[-1])
+print(s[3:])
+print(s[:-4])
+
+# formated Strings
+fname = "Nuwani"
+sname = "Sithara"
+
+print("My name is " + fname + " " + sname)
+
+v = f'My name is {fname} {sname}'  # Formated strings
+
+print(v)
+
+# -----------------------------------------------------
+# String methods
+
---
+++ test4.py
@@ -0,0 +1,4 @@
+
+a = input("Enter your birth year : ")
+b = input("Enter last 7 digits of your NIC : ")
+print( '"Your NIC number is : ' + a[2:] + b + "V" )
